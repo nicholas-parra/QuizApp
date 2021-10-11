12b21a79ed1219d6479e597ec7aa2f35a219d050
nicholasparra5 <nick.parra555@gmail.com>
2021-10-11 09:37:42 -0700

    Working on localization, serial console error occured, uploaded to send to Mr. Shorr for testing.

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.Group
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
-
     lateinit var game : Quiz
     lateinit var questionText : TextView
     lateinit var answerOne : Button
@@ -23,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var finalScore : TextView
     lateinit var groupMainUi : Group
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,19 +31,26 @@ class MainActivity : AppCompatActivity() {
         // open questions.json, read text and save to jsonText
         val inputStream = resources.openRawResource(R.raw.questions)
         val jsonText = inputStream.bufferedReader().use {
-            // the last line of the use function is returned -> it returns it.readText()
-            it.readText() // remember this is a LAMBDA, use BRACES
-        }
+            // the last line of the use lambda is returned -> lambda returns it.readText()
+            it.readText()
+        } // remember this is a LAMBDA, use BRACES
 
         // use gson to convert json into a List<Question>
         val sType = object : TypeToken<List<Question>>() { }.type
         val questions = Gson().fromJson<List<Question>>(jsonText, sType)
         game = Quiz(questions)
 
-        // setup each button's onClickListener to allow the game to run
+        // setup each button's onClickListener function to allow the game to run
         for (i in 0..3) {
             answerButtons[i].setOnClickListener {
-                game.isCorrect(i)
+                val currentQuestion = game.getCurrentQuestion()
+                var response = if(game.isCorrect(i)) {
+                    resources.getString(R.string.correct_response)
+                } else {
+                    resources.getString(R.string.false_response) +
+                            "\"${currentQuestion.answers[currentQuestion.correctAnswer]}\""
+                }
+                Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()
                 if (game.questionsRemaining()) {
                     updateText()
                 } else {
@@ -52,24 +59,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // update each button/textview for the first time, game functionality separate from this call
+        // functionality depends on onClickListeners
         updateText()
     }
 
-
     fun updateText() {
-        var currentQuestion = game.currentQuestion()
+        val currentQuestion = game.getCurrentQuestion()
         questionText.text = currentQuestion.question
         for (i in 0..3) {
             answerButtons[i].text = currentQuestion.answers[i]
         }
-
-        currentScore.text = "Current Score: ${game.score}"
+        currentScore.text = resources.getString(R.string.current_score) + game.score
     }
-
 
     fun reportScore() {
         groupMainUi.visibility = View.GONE
-        finalScore.text = "Final Score: ${game.score}"
+        finalScore.text = resources.getString(R.string.final_score) +
+                "${game.score}/${game.getQuestionNumber()}"
         finalScore.visibility = View.VISIBLE
     }
 
--- app/src/main/java/com/example/quizapp/Quiz.kt
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -1,32 +1,33 @@
 package com.example.quizapp
 
-import android.util.Log
-
 class Quiz(
     private val questionList : List<Question>,
     private var currentQuestionIndex : Int = 0,
     var score : Int = 0
     ) {
 
+    fun getQuestionNumber() : Int {
+        return questionList.size
+    }
 
     //Have a function to see if there are any more questions remaining, returning a boolean
     fun questionsRemaining() : Boolean {
-        return currentQuestionIndex < this.questionList.size
+        return currentQuestionIndex < questionList.size
     }
 
     //Have a function to return the current question object
-    fun currentQuestion() : Question {
+    fun getCurrentQuestion() : Question {
         return questionList[currentQuestionIndex]
     }
 
     /* Have a function to check the right answer(with a parameter of the selected answer choice),
     update the score, and return a String message about whether the player was right */
-    fun isCorrect(playerAnswer : Int) : String {
-        var retval = "Correct!"
-        if (playerAnswer == currentQuestion().correctAnswer) {
+    fun isCorrect(playerAnswer : Int) : Boolean {
+        var retval = true
+        if (playerAnswer == getCurrentQuestion().correctAnswer) {
             score++
         } else {
-            retval = "False!"
+            retval = false
         }
         currentQuestionIndex++
         return retval
